e6f5417d13112ab436076974a408bab30312e4db
kroll.anuar92@gmail.com <kroll.anuar92@gmail.com>
2017-09-02 10:34:26 +0900

    tested my variable

--- proxy.sh
+++ proxy.sh
@@ -1,4 +1,24 @@
-#!/bin/sh
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: proxy.sh
+# 
+#         USAGE: ./proxy.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 2017年09月02日 10:18
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
 
 
 
@@ -16,30 +36,40 @@ proxy=$(json_parser proxy_info ip_address) #; [ -z "$proxy" ];
 p_user_name=$(json_parser proxy_info user_name)
 p_password=$(json_parser proxy_info password)
  
-#if [ -n "$proxy" -o -n "$user_name" -a -n "password"  ];then
+#if [ -n "$proxy" -o -n "$p_user_name" -a -n "$password"  ];then
 if [ -n "$proxy" ];then
 	echo true
-	if [ -z "$user_name" -a -z "password" ]: then
+	if [ -z "$p_user_name" -a -z "$p_password" ]; then
 	 echo "no pass and username"
        	 #put log here
       	 run=1
-	else if [ -n "user_name"   ]
+	elif [ -n "$p_user_name" ]; then
 	 echo "there are username"
 	 run=0
-	else if [ -n "password" ] 
+	elif [ -n "$p_password" ] ; then
 	 echo "there are no password"
 	 run=0
 	fi
-else 
- 	echo "no proxy"
-	run=3	
+#else 
+#  	echo no "
+# 	run=3	
 fi
-
-
-case $run in 
-	1) curl 
-	   ;;
-	2) curl
-	   ;;
-	3) curl
-	   ;;
+# 
+# 
+# case $run in 
+# 	1) curl 
+# 	   ;;
+# 	2) curl
+# 	   ;;
+# 	3) curl
+# 	   ;;
+# 
+# #proxy with identifier 
+# #proxy_identifier parameter
+# #0 = no proxy
+# # 1 = proxy with only ip (no Auth)
+# # 2 = proxy with auth(username and passwor)
+# # 3 = proxy with username
+# # 4 = proxy with password
+# proxy identifier=$(json_parser proxy_info proxy_identifier) 
+ 
